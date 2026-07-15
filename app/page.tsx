32f328058c4123c5941296b902135d8eecee6896
LimDong <ilhai6@naver.com>
2026-07-15 11:09:07 +0900

    fix: 메인 페이지 코드 제거

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,3 @@
-"use client";
-
-import Popup from "@/components/common/Modal/Popup";
-import LinkModal from "@/components/common/Modal/LinkModal";
-import PluginModal from "@/components/common/Modal/PluginModal/PluginModal";
-import SettingModal from "@/components/common/Modal/SettingModal/SettingModal";
-import TaskModal from "@/components/common/Modal/TaskModal/TaskModal";
-import Chips from "@/components/common/Chips";
-
 export default function Home() {
-  return (
-    <div className="flex flex-col gap-2.5 md:gap-3">
-      <Popup onDelete={() => {}} />
-      <LinkModal onConfirm={() => {}} />
-      <PluginModal onConfirm={() => {}} />
-      <SettingModal onConfirm={() => {}} />
-      <TaskModal />
-      <Chips variant="to do" />
-      <Chips variant="done" />
-    </div>
-  );
+  return <div></div>;
 }
