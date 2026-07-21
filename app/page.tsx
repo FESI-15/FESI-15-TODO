5157e82262b2768979be2850eedb1635222d1c5b
LimDong <ilhai6@naver.com>
2026-07-21 14:15:37 +0900

    fix: 메인페이지 코드 삭제

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,3 @@
-"use client";
-import { usePostAuthLogin } from "@/hooks/queries/auth.bff.hook";
-
 export default function Home() {
-  const { mutate } = usePostAuthLogin();
-  return (
-    <div>
-      <form
-        action={() =>
-          mutate({
-            data: { email: "ilhai3@naver.com", password: "sks98sk11" },
-          })
-        }
-      >
-        <input type="text" name="email" />
-        <input type="password" name="password" />
-        <button type="submit">Login</button>
-      </form>
-    </div>
-  );
+  return <div></div>;
 }
