b485adc751a1c6746a6614053aef979adc4f7ea5
minyeong123 <jmy0483@naver.com>
2026-08-11 14:51:38 +0900

    fix: 리뷰 반영

--- components/dashboard/TaskColumn/TaskColumn.tsx
+++ components/dashboard/TaskColumn/TaskColumn.tsx
@@ -16,7 +16,7 @@ export default function TaskColumn({
   return (
     <div
       className={cn(
-        "min-h-[324px] min-w-0 flex-1 rounded-3xl p-4 lg:p-6 flex flex-col",
+        "min-w-0 flex-1 rounded-3xl p-4 lg:p-6 flex flex-col",
         done ? "bg-white dark:bg-card" : "bg-orange-100 dark:bg-[#2e2720]",
       )}
     >
