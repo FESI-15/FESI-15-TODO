17df4bc947d8baf1ab85549d480b5ec664d663f4
LimDong <ilhai6@naver.com>
2026-08-11 11:09:52 +0900

    feat: 소통 상세 스켈레톤 반응형 개발

--- app/(with-header)/community/[id]/page.tsx
+++ app/(with-header)/community/[id]/page.tsx
@@ -24,59 +24,7 @@ export default async function CommunityDetailPage({
 
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
-      {/* <CommunityDetail id={Number(id)} /> */}
-      <div className="p-4">
-        <div className="w-full bg-white rounded-[24px] py-6 px-4 max-w-[768px] mx-auto">
-          <div className="flex flex-col gap-6">
-            <Skeleton className="w-[200px] h-6 " />
-            <Skeleton className="w-[66px] h-5 " />
-          </div>
-          <div className="mt-10">
-            <div className="flex flex-col gap-2">
-              <Skeleton className="w-[300px] h-6 " />
-              <Skeleton className="w-[250px] h-6 " />
-              <Skeleton className="w-[200px] h-6 " />
-            </div>
-            <div className="mt-6">
-              <Skeleton className="size-[150px] " />
-            </div>
-            <Skeleton className="w-[114px] h-4 mt-4 " />
-          </div>
-          <div className="mt-10">
-            <Skeleton className="w-10 h-6 " />
-            <div className="flex gap-3 mt-4 justify-between">
-              <Skeleton className="w-full h-11 " />
-              <Skeleton className="w-16 h-10 " />
-            </div>
-            <div className="mt-6 flex flex-col gap-8">
-              <div>
-                <Skeleton className="w-[56px] h-5 " />
-                <div className="mt-3">
-                  <Skeleton className="w-[250px] h-5 " />
-                  <Skeleton className="w-[200px] h-5 mt-1 " />
-                  <Skeleton className="w-[62px] h-4 mt-2 " />
-                </div>
-              </div>
-              <div>
-                <Skeleton className="w-[56px] h-5 " />
-                <div className="mt-3">
-                  <Skeleton className="w-[250px] h-5 " />
-                  <Skeleton className="w-[200px] h-5 mt-1 " />
-                  <Skeleton className="w-[62px] h-4 mt-2 " />
-                </div>
-              </div>
-              <div>
-                <Skeleton className="w-[56px] h-5 " />
-                <div className="mt-3">
-                  <Skeleton className="w-[250px] h-5 " />
-                  <Skeleton className="w-[200px] h-5 mt-1 " />
-                  <Skeleton className="w-[62px] h-4 mt-2 " />
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <CommunityDetail id={Number(id)} />
     </HydrationBoundary>
   );
 }
