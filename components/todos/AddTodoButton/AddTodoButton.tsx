eca2de0a96bd194452327c6cc35c6037e0aee37a
LimDong <ilhai6@naver.com>
2026-08-12 14:43:12 +0900

    fix: 코드리뷰 반영

--- components/todos/AddTodoButton/AddTodoButton.tsx
+++ components/todos/AddTodoButton/AddTodoButton.tsx
@@ -16,7 +16,7 @@ export default function AddTodoButton() {
       <button
         type="button"
         className="inline-flex items-center gap-2 text-gray-500 dark:text-muted-foreground py-2.5 px-3 border border-gray-300 dark:border-border rounded-full hover:text-gray-600 hover:border-[#bbb] dark:hover:text-foreground dark:hover:border-muted-foreground"
-        onClick={() => setOpen(false)}
+        onClick={() => setOpen(true)}
       >
         <PlusIcon className="size-5" />
         <p className="text-sm font-semibold">할 일 추가</p>
