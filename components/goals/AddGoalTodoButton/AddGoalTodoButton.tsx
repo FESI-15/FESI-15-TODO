53b4ebc2e14c4ceb902e35cec02b7e106dad2be5
LimDong <ilhai6@naver.com>
2026-08-13 13:35:09 +0900

    feat: 모달 최적화

--- components/goals/AddGoalTodoButton/AddGoalTodoButton.tsx
+++ components/goals/AddGoalTodoButton/AddGoalTodoButton.tsx
@@ -1,27 +1,43 @@
-import TaskFormModal from "@/components/common/Modal/TaskFormModal/TaskFormModal";
 import PlusIcon from "@/public/icons/common/plus.svg";
+import dynamic from "next/dynamic";
+import { useState } from "react";
 
 interface AddGoalTodoButtonProps {
   goalId: number;
 }
 
+const TaskFormModal = dynamic(
+  () => import("@/components/common/Modal/TaskFormModal/TaskFormModal"),
+  {
+    ssr: false,
+  },
+);
+
 export default function AddGoalTodoButton({ goalId }: AddGoalTodoButtonProps) {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <div>
-      <TaskFormModal
-        defaultValues={{
-          title: "",
-          goalId,
-          dueDate: "",
-          linkUrl: "",
-          tags: [],
-          fileUrl: "",
-        }}
+      <button
+        type="button"
+        onClick={() => setIsOpen(true)}
+        className="inline-flex items-center gap-2 rounded-full bg-orange-500 px-4 py-2 text-sm font-semibold text-white hover:bg-orange-600"
       >
-        <div className="inline-flex items-center gap-2 rounded-full bg-orange-500 px-4 py-2 text-sm font-semibold text-white hover:bg-orange-600">
-          <PlusIcon className="size-4" />할 일 추가
-        </div>
-      </TaskFormModal>
+        <PlusIcon className="size-4" />할 일 추가
+      </button>
+      {isOpen && (
+        <TaskFormModal
+          open={isOpen}
+          onOpenChange={setIsOpen}
+          defaultValues={{
+            title: "",
+            goalId,
+            dueDate: "",
+            linkUrl: "",
+            tags: [],
+            fileUrl: "",
+          }}
+        />
+      )}
     </div>
   );
 }
