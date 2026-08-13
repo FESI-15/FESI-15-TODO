9f6cfe9c45e04bf99154216a382cb3765f746b14
minyeong123 <jmy0483@naver.com>
2026-08-13 16:29:03 +0900

    refactor: 마이페이지 성능최적화, 언어 삭제

--- components/mypage/ProfileImageInput.tsx
+++ components/mypage/ProfileImageInput.tsx
@@ -74,6 +74,7 @@ export function ProfileImageInput<T extends FieldValues>({
           height={132}
           unoptimized={displayUrl.startsWith("blob:")}
           className="size-full object-cover"
+          priority
         />
       </div>
       <button
