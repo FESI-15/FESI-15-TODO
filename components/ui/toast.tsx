70c1c90c25d5867580081a35a717c18412efc8ce
LimDong <ilhai6@naver.com>
2026-08-18 13:32:14 +0900

    fix: toast 위치, 크기 조정

--- components/ui/toast.tsx
+++ components/ui/toast.tsx
@@ -22,7 +22,7 @@ const toastVariants = cva(
     "group/toast pointer-events-auto relative w-full border shadow-lg outline-none",
     "transition-all duration-300 ease-[cubic-bezier(0.22,1,0.36,1)]",
     "hover:-translate-y-0.5 hover:shadow-xl",
-    "data-starting-style:opacity-0 data-starting-style:translate-y-8",
+    "data-starting-style:opacity-0 data-starting-style:translate-x-8",
     "data-ending-style:opacity-0 data-ending-style:scale-95",
   ].join(" "),
   {
@@ -57,7 +57,7 @@ function ToastViewport({ className, ...props }: ToastPrimitive.Viewport.Props) {
     <ToastPrimitive.Viewport
       data-slot="toast-viewport"
       className={cn(
-        "pointer-events-none fixed right-8 bottom-0 z-50 flex w-[500px] flex-col gap-3 py-10 outline-none",
+        "pointer-events-none fixed md:right-8 md:bottom-8 right-4 bottom-4 z-50 flex w-[300px] md:w-[500px] flex-col gap-3 outline-none",
         className,
       )}
       {...props}
@@ -142,7 +142,7 @@ function ToastClose({
       aria-label="Close toast"
       render={render}
       className={cn(
-        "relative shrink-0 text-white after:absolute after:-inset-2 hover:text-white",
+        "relative shrink-0 text-white after:absolute after:-inset-2 hover:bg-transparent",
         className,
       )}
       {...props}
